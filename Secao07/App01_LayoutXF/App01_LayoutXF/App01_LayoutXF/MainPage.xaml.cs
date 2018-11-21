3a032594415b31693179db8ae1385ac4b2d58267
Fabrício Barbosa <fabriciopbrb@gmail.com>
2018-11-21 08:39:26 -0200

    Início Layouts - criacao da base

---
+++ Secao07/App01_LayoutXF/App01_LayoutXF/App01_LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_LayoutXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void GoPageStack(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Stack.StackPage());
+        }
+        private void GoPageAbsolute(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+        }
+        private void GoPageRelative(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Relative.RelativePage());
+        }
+        private void GoPageScroll(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Scroll.ScrollPage());
+        }
+        private void GoPageGrid(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Grid.GridPage());
+        }
+    }
+}
