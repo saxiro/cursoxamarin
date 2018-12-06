fc4b7b29909af478951420b2e7480564797db612
Fabrício Barbosa <fabriciopbrb@gmail.com>
2018-12-06 16:03:32 -0200

    Banco de Dados em Implementação

---
+++ Secao11/App1_Vagas/App1_Vagas/App1_Vagas/Banco/Database.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App1_Vagas.Modelos;
+using Xamarin.Forms;
+using SQLite;
+
+namespace App1_Vagas.Banco
+{
+    public class Database
+    {
+        private SQLiteConnection _conexao;
+
+
+        public Database()
+        {
+            var dep = DependencyService.Get<ICaminho>();
+            string caminho = dep.ObterCaminho("database.sqlite");
+            
+            _conexao = new SQLiteConnection(caminho);
+            _conexao.CreateTable<Vaga>();
+        }
+        public void Cadastro(Vaga vaga)
+        {
+        }
+        public List<Vaga> Consultar()
+        {
+            return null;
+        }
+        public Vaga ObterVagaPorId(int id)
+        {
+            return null;
+        }
+        public void Atualizacao(Vaga vaga)
+        {
+
+        }
+        public void Exclusao(int id)
+        {
+
+        }
+    }
+}
---
+++ Secao11/App1_Vagas/App1_Vagas/App1_Vagas/Modelos/Vaga.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace App1_Vagas.Modelos
+{
+    [Table("Vaga")]
+    public class Vaga
+    {
+        [Primarykey, AutoIncrement]
+        public int Id { get; set; }
+        public string NomeVaga { get; set; }
+        public short Quantidade { get; set; }
+        public String Cidade { get; set; }
+        public Double Salario { get; set; }
+        public string Descricao { get; set; }
+        public string TipoContratacao { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
