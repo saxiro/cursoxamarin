42abd41b1a9dd150fa29a70637ef1b94bd382afd
Fabrício Barbosa <fabriciopbrb@gmail.com>
2019-02-07 21:42:43 -0200

    Curso Baltazar

---
+++ SpaUserControl/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using SpaUserControl.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("André", "andrebaltieri@hotmail.com");
+            
+
+            try
+            {
+                user.SetPassword("andreBaltiori", "andreBaltiori");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(user.Name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SpaUserControl/SpaUserControl.Domain/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using SpaUserControl.Common.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaUserControl.Domain.Models
+{
+    public class User
+    {
+        public User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public void SetPassword(string password, string confirmPassword)
+        {
+            AssertionConcern.AssertArgumentNotNull(password, "Senha Inválida");
+            AssertionConcern.AssertArgumentNotNull(confirmPassword, "Confirmação Inválida");
+            AssertionConcern.AssertArgumentEquals(password, confirmPassword, "A senha e confirmação não coincidem.");
+            AssertionConcern.AssertArgumentLength(password, 6, 20, "A senha deve conter entre 6 e 20 caracteres");
+            Password = password;
+        }
+    }
+}
